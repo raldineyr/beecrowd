b025f2f2bfd7f58c588b5982539194dcbce68fd0
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-05-11 07:46:10 -0300

    1005 - Média 1

---
+++ Linguagem C/1005.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float A,B, MEDIA;
+    scanf("%f",&A);
+    scanf("%f",&B);
+    A=3.5*A;
+    B=7.5*B;
+    MEDIA=(A+B)/11;
+    printf("MEDIA = %.5f\n",MEDIA);
+    return 0;
+}
