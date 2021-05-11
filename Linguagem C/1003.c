a31001fb8b9e95a8dd620ad780d1baddb299be4d
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-05-11 07:41:06 -0300

    1003 - Soma Simples

---
+++ Linguagem C/1003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int A, B;
+   scanf("%d",&A);
+   scanf("%d",&B);
+   int SOMA = A + B;
+
+   printf("SOMA = %d\n",SOMA);
+    return 0;
+}
