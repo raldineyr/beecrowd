a416511020925dd9b7ff9b3d95353ae3431e7470
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-05-25 06:35:55 -0300

    1759 - Ho Ho Ho

---
+++ Linguagem C/1759.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    int n,x;
+    
+    scanf("%d",&n);
+    x=n;
+    if (x > 0){
+    for (x = 2; x <= n; x++){
+        printf("Ho ");
+    }
+    printf("Ho!\n");
+  }
+    
+    return 0;
+}
