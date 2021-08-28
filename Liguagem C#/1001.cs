c8dbd6ddf24514069f3132c398fecd27f3c9908d
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-28 20:45:25 -0300

    1001 - Extremamente Básico

---
+++ Liguagem C#/1001.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+     
+        Console.Write("X = {0}\n",A + B);
+    }
+
+}
