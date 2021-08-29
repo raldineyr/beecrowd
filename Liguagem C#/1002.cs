a4028c5499ea26d195d7e2cf0439fad7e9896463
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-29 09:00:37 -0300

    1002 - Área do Círculo

---
+++ Liguagem C#/1002.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        
+        double raio = double.Parse(Console.ReadLine());
+        Console.Write("A={0:F4}\n",raio*raio*3.14159);
+    
+    }
+
+}
