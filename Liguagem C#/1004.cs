ada6abd30ce0e8bbad59089920d9d7076c03cb60
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-31 07:44:15 -0300

    1004 - Produto Simples

---
+++ Liguagem C#/1004.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+    int A = int.Parse(Console.ReadLine());
+    int B = int.Parse(Console.ReadLine());
+    
+    Console.Write("PROD = {0}\n", A*B);
+
+    }
+
+}
