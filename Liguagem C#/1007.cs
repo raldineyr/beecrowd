0262b44e86276d631fa46103e5567313afe0622e
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-09-03 06:50:14 -0300

    1007 - Diferença

---
+++ Liguagem C#/1007.cs
@@ -0,0 +1,16 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+    int A = int.Parse(Console.ReadLine());
+    int B = int.Parse(Console.ReadLine());
+    int C = int.Parse(Console.ReadLine());
+    int D = int.Parse(Console.ReadLine());
+    
+    Console.Write("DIFERENCA = {0}\n",(A*B)-(C*D));
+
+    }
+
+}
