eae64c402952c83951108e9863dcfef8ca8bbdf4
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-09-13 08:03:29 -0300

    1016 - Distância

---
+++ Liguagem C#/1016.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+    
+    int quilometro = int.Parse(Console.ReadLine());
+    
+    Console.Write("{0} minutos\n",(60*quilometro)/30);
+
+    }
+
+}
